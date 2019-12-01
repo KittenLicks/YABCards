5cf6a29ac557be2e699a1d706f0350c66be8e17b
KittenLicks <30407314+KittenLicks@users.noreply.github.com>
2019-12-01 21:34:35 +0200

    Add files via upload

---
+++ drawing.js
@@ -0,0 +1,99 @@
+var gif = new GIF({
+  workers: 2,
+  quality: 10,
+  repeat:0,
+  height:165,
+  width:400
+});
+
+let edit = document.getElementById('editcanvas')
+let ectx = edit.getContext('2d')
+
+splash = []
+
+function loadSplash()
+{
+	for (let i=0;i<17;i++)
+	{
+		let file = "images/"+(i+1)+".png"
+		splash[i] = new Image()
+		splash[i].src = file
+	}
+}
+function makeGif()
+{
+	for (i in splash)
+	{
+		//Start with the background
+		ectx.clearRect(0,0,edit.width,edit.height)
+		ectx.drawImage(splash[i],0,0)
+
+		//Image	
+		let xoffset = parseInt(document.getElementById('objectionOffsetX').value)
+		let yoffset = parseInt(document.getElementById('objectionOffsetY').value)
+		let zoffset = parseInt(document.getElementById('objectionOffsetZ').value)
+
+		let zoom = zoffset / 10
+
+		let midx = object.width /2 - (objectionImage.width * zoom) /2 
+		let midy = object.height /2 - (objectionImage.height * zoom) /2 
+
+		ectx.drawImage(objectionImage, midx +xoffset, midy +yoffset, objectionImage.width * zoom, objectionImage.height * zoom)
+
+		//Add some black bars
+		ectx.fillStyle = "black"
+		ectx.fillRect(0,0,edit.width,barSize)
+		ectx.fillRect(0,edit.height - barSize,edit.width,barSize)
+		
+		//Text
+		objectoffset = 35
+
+		textTopOffset = parseInt(document.getElementById('topSlider').value)
+		textBottomOffset = parseInt(document.getElementById('bottomSlider').value)
+
+		let text = document.getElementById('objectiontop').value
+		ectx.font = "32px Bangers"
+		ectx.fillStyle = "white"
+		mid = object.width/2 - ectx.measureText(text).width/2
+		ectx.fillText(text,mid + textTopOffset, objectoffset)
+
+		text = document.getElementById('objectionbottom').value
+		ectx.font = "32px Bangers"
+		ectx.fillStyle = "white"
+		mid = object.width/2 - octx.measureText(text).width/2
+		ectx.fillText(text,mid + textBottomOffset, object.height - objectoffset / 4)
+
+		addFrame(ectx,100)
+	}
+
+	gif.on('finished', function(blob){
+			saveImage(blob)
+		});
+	gif.render();
+}
+function saveImage(blob)
+{	
+	saveBlobAsFile(blob,"objection.gif");
+}
+function addFrame(c, delay){
+	gif.addFrame(c,{copy:true, delay:delay})
+}
+
+function saveBlobAsFile(blob, fileName) {
+
+    var reader = new FileReader();
+
+    reader.onloadend = function () {    
+        var base64 = reader.result ;
+        var link = document.createElement("a");
+
+        link.setAttribute("href", base64);
+        link.setAttribute("download", fileName);
+        link.click();
+    };
+
+    reader.readAsDataURL(blob);
+}
+
+loadSplash()
+
--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let card = document.getElementById('card')
 let ctx = card.getContext("2d");
+
+let object = document.getElementById('objectionexample')
+let octx = object.getContext('2d')
 //Set up fonts
 topFont = "62px Calligraffitti"
 middleFont = "34px Open Sans"
 bottomFont = "bold 38px Roboto"
+objectionFont = "Bangers"
+
+//Background vars
+let barSize = 46
 
 //Load background
 blank = new Image()
@@ -72,11 +79,77 @@ function clearCanvas()
 {
 	ctx.clearRect(0,0,card.width,card.height)
 }
+objectionFrame = 0
 function update()
 {
 	let topText = document.getElementById('top').value;
 	let middleText = document.getElementById('middle').value;
 	let bottomText = document.getElementById('bottom').value;
 	drawCard(topText,middleText,bottomText)
+
+	updateObjection()
+}
+let objectionImage = new Image();
+function updateImage()
+{
+	let file = document.getElementById('chooseImage').files[0]
+	if (file)
+	{		
+		objectionImage.file = file;
+		
+		const reader = new FileReader();
+	    reader.onload = function(e) { 
+	    	objectionImage.src = e.target.result; 
+	    }; 
+
+    	reader.readAsDataURL(file);
+	}
+}
+function updateObjection()
+{
+
+	//Background
+	objectionFrame++
+	objectionFrame = objectionFrame >= splash.length ? 0 : objectionFrame; //Wrap around to 0
+
+	octx.clearRect(0,0,object.width,object.height)
+	octx.drawImage(splash[objectionFrame],0,0)
+
+	//Image
+	
+	let xoffset = parseInt(document.getElementById('objectionOffsetX').value)
+	let yoffset = parseInt(document.getElementById('objectionOffsetY').value)
+	let zoffset = parseInt(document.getElementById('objectionOffsetZ').value)
+
+	let zoom = zoffset / 10
+
+	let midx = object.width /2 - (objectionImage.width * zoom) /2 
+	let midy = object.height /2 - (objectionImage.height * zoom) /2 
+
+	octx.drawImage(objectionImage, midx +xoffset, midy +yoffset, objectionImage.width * zoom, objectionImage.height * zoom)
+
+	//Draw bars
+	octx.fillStyle = "black"
+	octx.fillRect(0,0,object.width,barSize)
+	octx.fillRect(0,object.height - barSize,object.width,barSize)
+
+	objectoffset = 35
+
+	textTopOffset = parseInt(document.getElementById('topSlider').value)
+	textBottomOffset = parseInt(document.getElementById('bottomSlider').value)
+
+	//Draw text
+	let text = document.getElementById('objectiontop').value
+	octx.font = "32px Bangers"
+	octx.fillStyle = "white"
+	mid = object.width/2 - octx.measureText(text).width/2
+	octx.fillText(text,mid + textTopOffset, objectoffset)
+
+	//Draw text
+	text = document.getElementById('objectionbottom').value
+	octx.font = "32px Bangers"
+	octx.fillStyle = "white"
+	mid = object.width/2 - octx.measureText(text).width/2
+	octx.fillText(text,mid + textBottomOffset, object.height - objectoffset / 4)
 }
 setInterval(update,100)
